db6092084817a253ecaf81d7d60cc32f6f198649
Shajeeahmed986 <154664386+Shajeeahmed986@users.noreply.github.com>
2024-03-02 22:39:01 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain.llms import OpenAI
 st.set_page_config(page_title="🦜🔗 Quickstart App")
-st.title('🦜🔗 Quickstart App')
+st.title('🔗 For Your Easiness')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
